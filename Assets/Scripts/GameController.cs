29d30aa1d8393d67bb2d03de200e08839ca7511a
Chris <sjhshy@gmail.com>
2017-06-24 17:52:52 +0900

    DialogManager 샘플

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		yield return new WaitForSeconds (1f);
+
+		DialogDataAlert alert = new DialogDataAlert ("Welcome", "Lets start!", delegate() {
+			Debug.Log("OK Pressed!");
+
+			DialogDataConfirm confirm = new DialogDataConfirm("Buy Gold?", "Do you want buy 100,000 golds?", 
+				delegate(bool yn) {
+					if(yn){
+						Debug.Log("Confirm OK");
+					}else{
+						Debug.Log("Confirm Cancel");
+						
+					}
+				
+			});
+			
+			DialogManager.Instance.Push(confirm);
+
+		});
+		DialogManager.Instance.Push (alert);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
